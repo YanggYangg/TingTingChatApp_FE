4922f20eed627b37ea8603c008eb848dec7d4542
21034451_NguyenThiQuynhGiang <nguyenquynhgiang1324@gmail.com>
2025-03-19 14:57:04 +0700

    Merge and fix render contact

--- tingting_web/src/App.jsx
+++ tingting_web/src/App.jsx
@@ -1,8 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DefaultLayout from './layouts/DefaultLayout';
-import ChatPage from './pages/Chat/ChatPage';
-import ContactsPage from './pages/Chat/ContactsPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React,  {useState } from "react";
+import DefaultLayout from "./layouts/DefaultLayout";
+import ChatPage from "./pages/Chat/ChatPage";
+import ContactsPage from "./pages/Chat/ContactsPage";
+
+import Contact from './layouts/components/contact-form/Contact';
 import ContactList from './layouts/components/contact-form/ContactList/ContactList';
+import GroupList from './layouts/components/contact-form/GroupList/GroupList';
+import FriendRequests from './layouts/components/contact-form/FriendRequests';
+import ChatList from "./layouts/components/chatlist";
+
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -11,16 +18,17 @@ function App() {
     <Provider store={store}>
       <Router>
         <Routes>
-                <Route path="/" element={<DefaultLayout />}>
-                    <Route index element={<h1>Chat window</h1>} />
-                    <Route path="chat" element={<ChatPage/>} />
-                    <Route path="contact" element={<ContactList/>} />
-                </Route>
-            </Routes>
-     </Router>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<h1>Chat window</h1>} />
+            <Route path="chat" element={<ChatPage />} />
+            <Route path="/contacts/:tab" element={<ContactsPage />} />
+        <Route path="*" element={<Navigate to="/contacts/friends" />} /> {/* Mặc định chuyển đến bạn bè */}
+              
+          </Route>
+        </Routes>
+      </Router>
     </Provider>
   );
 }
 
 export default App;
-
