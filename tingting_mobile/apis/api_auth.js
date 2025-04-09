909d3c1e870884a8e06f26ae9b3edae88710c69c
21034451_NguyenThiQuynhGiang <nguyenquynhgiang1324@gmail.com>
2025-04-10 01:31:23 +0700

    install axios

--- tingting_mobile/apis/api_auth.js
+++ tingting_mobile/apis/api_auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.1.2:3000';
+const BASE_URL = 'http://192.168.0.104:3000';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
