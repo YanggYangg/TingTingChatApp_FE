377367fd53ab4191d1e88437edd567c4eee01fec
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-04-14 02:44:07 +0700

    change BASE_URL

--- tingting_mobile/apis/ApiManager.js
+++ tingting_mobile/apis/ApiManager.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://192.168.139.71:5000";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
