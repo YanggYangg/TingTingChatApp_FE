bfb713be1f1e2a85af02baf824c8280e7698c967
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-05-07 22:03:05 +0700

    change port

--- tingting_mobile/apis/api_s3.js
+++ tingting_mobile/apis/api_s3.js
@@ -12,7 +12,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE_URL = "http://192.168.1.7:5000";
+const BASE_URL = "http://192.168.1.8:5000";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
