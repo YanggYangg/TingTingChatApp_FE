4a3f112e07160268913a292bb488824767df8533
21034451_NguyenThiQuynhGiang <nguyenquynhgiang1324@gmail.com>
2025-05-26 10:42:07 +0700

    fix

--- tingting_web/vite.config.ts
+++ tingting_web/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       stream: "stream-browserify",
       buffer: "buffer",
     },
+     mainFields: ["module", "main"], 
   },
   optimizeDeps: {
     esbuildOptions: {
